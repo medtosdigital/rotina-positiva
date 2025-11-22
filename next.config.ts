a35ef6b976a7eac9de7cfd9874db0702f03f9469
MEDTOS DIGITAL <medtosdigital@gmail.com>
2025-11-22 04:51:53 +0000

    Ajustes finais na landing page

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  assetPrefix: './',
   typescript: {
     ignoreBuildErrors: true,
   },
